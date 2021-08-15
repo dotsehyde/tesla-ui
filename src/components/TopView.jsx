3ff7f629a318ebf7933d9f19a2588d0c91716d03
Benjamin <dotsehyde@gmail.com>
2021-08-15 16:49:59 +0000

    :0 75%

---
+++ src/components/TopView.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function TopView() {
+    return (
+        <section class="d-flex align-items-md-end" data-aos="fade-up" data-aos-duration="850" data-aos-once="true" id="topview">
+        <div class="container d-flex d-sm-flex align-items-end align-items-sm-end" style={{marginBottom: "150px"}}>
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-6">
+                    <h3>Glass Roof</h3>
+                    <p>A lightweight, removable Glass Roof stores in the trunk for an open-air, convertible driving experience.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    )
+}
+
+export default TopView
